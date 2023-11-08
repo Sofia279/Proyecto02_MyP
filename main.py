2a2efc6ea68f51b4582a970ce612d5ad7d1f31a4
Sofia279 <72100947+Sofia279@users.noreply.github.com>
2023-11-07 21:04:48 -0600

    Correccion de identacion

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def main():
                         action="store_true")
     args = parser.parse_args()
 
-for fname in args.images:
-    if not fname.exists():
-        print(f"No se encontró el archivo {fname}")
-        continue
-    img = CloudImage(fname)
-    if args.save:
-        img.write_conv()
-    print(f"{img.name}: {img.cloud_cov}")
-if __main__ == "__main__":
+    for fname in args.images:
+        if not fname.exists():
+            print(f"No se encontró el archivo {fname}")
+            continue
+        img = CloudImage(fname)
+        if args.save:
+            img.write_conv()
+        print(f"{img.name}: {img.cloud_cov}")
+if __name__ == "__main__":
     main()
     
