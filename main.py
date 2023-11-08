4db433ffd806ec0ae36aa58d018d2b21268a6c6d
icmor <cornejodlm@ciencias.unam.mx>
2023-11-07 20:21:00 -0600

    Arreglar formato
    Límite de longitud de línea 79

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import pathlib
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("images", help="archivos de imagenes", nargs='+', type=pathlib.Path)
-    parser.add_argument("-s", "-S", "--save", help="escribir archivos procesados con sufijo -seg", action="store_true")
+    parser.add_argument("images", help="archivos de imagenes",
+                        nargs='+', type=pathlib.Path)
+    parser.add_argument("-s", "-S", "--save",
+                        help="escribir archivos procesados con sufijo -seg",
+                        action="store_true")
     args = parser.parse_args()
     images = []
 
